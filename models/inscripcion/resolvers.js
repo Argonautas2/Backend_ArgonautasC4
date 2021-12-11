f67b23a17c7982ddaea97a7f3bca18b0c3a0e8a6
SandraCaballeroV <90484317+SandraCaballeroV@users.noreply.github.com>
2021-12-10 20:56:26 -0500

    Add files via upload

---
+++ models/inscripcion/resolvers.js
@@ -0,0 +1,38 @@
+import { InscriptionModel } from './inscripcion.js';
+import { ProjectModel } from '../proyecto/proyecto.js';
+
+const resolverInscripciones = {
+  Inscripcion: {
+    proyecto: async (parent, args, context) => {
+      return await ProjectModel.findOne({ _id: parent.proyecto });
+    },
+  },
+  Query: {
+    Inscripciones: async (parent, args) => {
+      const inscripciones = await InscriptionModel.find();
+      return inscripciones;
+    },
+  },
+  Mutation: {
+    crearInscripcion: async (parent, args) => {
+      const inscripcionCreada = await InscriptionModel.create({
+        estado: args.estado,
+        proyecto: args.proyecto,
+        estudiante: args.estudiante,
+      });
+      return inscripcionCreada;
+    },
+    aprobarInscripcion: async (parent, args) => {
+      const inscripcionAprobada = await InscriptionModel.findByIdAndUpdate(
+        args.id,
+        {
+          estado: 'ACEPTADO',
+          fechaIngreso: Date.now(),
+        },
+        { new: true }
+      );
+      return inscripcionAprobada;
+    },
+  },
+};
+export { resolverInscripciones };
